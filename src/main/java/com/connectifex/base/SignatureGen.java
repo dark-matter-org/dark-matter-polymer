d42512dc14b7881a4f6077a2f145812dc408c776
Peter Strong <darkmatter@connectifex.com>
2020-08-28 16:01:56 -0400

    Adding Apache license to extended code
    Removing unneeded code.

--- src/main/java/com/connectifex/base/SignatureGen.java
+++ src/main/java/com/connectifex/base/SignatureGen.java
@@ -1,3 +1,18 @@
+// Copyright 2020 connectifex
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
 package com.connectifex.base;
 
 import java.io.BufferedReader;
@@ -156,6 +171,7 @@ public class SignatureGen {
 	 * @param obj the object to be scanned
 	 * @return a string with keys, separated by |
 	 */
+	@SuppressWarnings("unused")
 	private String getPotentialKeys(JSONObject json) {
 		StringBuilder rc = new StringBuilder();
 
